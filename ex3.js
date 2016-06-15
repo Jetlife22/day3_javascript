947a628d1eaba7f9ea804274262c8355ba3debea
GregC91 <g_chambers1801@icloud.com>
2016-06-15 13:54:51 +0100

    delete and add functions
    specifically, it's the shift and unshift to the array members in the
    array properties

---
+++ ex3.js
@@ -0,0 +1,18 @@
+//declaring two variables each for a separate function (delete1 in JS = deletButton in HTML)
+var deleteEvent = document.getElementById("deleteButton");
+delete1.addEventListener('click', myDelete,false);
+
+//declaring two variables each for a separate function (add1 in JS = addButton in HTML)
+var addEvent = document.getElementById("addButton");
+add1.addEventListener('click', myAdd,false);
+
+var people = []
+
+people = ['ronny', 'bobby', 'ricky', 'mike']
+
+document.getElementById("demo").innerHTML = people;
+
+function myDelete ()
+{
+    
+}
