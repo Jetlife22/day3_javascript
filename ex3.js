d1f8a879947f83c8a4b8dba2f271c30ac0392745
GregC91 <g_chambers1801@icloud.com>
2016-06-15 14:20:25 +0100

    Figured it out
    Did a separate function for each button to unshift, shift, push and pop,
    all have their own button and actions.

--- ex3.js
+++ ex3.js
@@ -1,18 +1,46 @@
-//declaring two variables each for a separate function (delete1 in JS = deletButton in HTML)
-var deleteEvent = document.getElementById("deleteButton");
-delete1.addEventListener('click', myDelete,false);
+//declaring  variables each for a separate function (shift = delete first = shiftButton in HTML)
+var shiftEvent = document.getElementById("shiftButton");
+shiftEvent.addEventListener('click', myShift,false);
 
-//declaring two variables each for a separate function (add1 in JS = addButton in HTML)
-var addEvent = document.getElementById("addButton");
-add1.addEventListener('click', myAdd,false);
+//declaring  variables each for a separate function (unshift = add first = unshiftButton in HTML)
+var unshiftEvent = document.getElementById("unshiftButton");
+unshiftEvent.addEventListener('click', myUnshift,false);
+
+//declaring two variables each for a separate function (pop = delete last = popButton in HTML)
+var popEvent = document.getElementById("popButton");
+popEvent.addEventListener('click', myPop,false);
+
+//declaring two variables each for a separate function (push = add last = pushButton in HTML)
+var pushEvent = document.getElementById("pushButton");
+pushEvent.addEventListener('click', myPush,false);
+
 
 var people = []
 
-people = ['ronny', 'bobby', 'ricky', 'mike']
+people = ['ronny', 'bobby', 'ricky', 'mike'];
 
 document.getElementById("demo").innerHTML = people;
 
-function myDelete ()
+function myShift ()
 {
-    
+   people.shift();
+   document.getElementById("demo").innerHTML = people;
+}
+
+function myUnshift ()
+{
+   people.unshift("New Edition");
+   document.getElementById("demo").innerHTML = people;
+}
+
+function myPop ()
+{
+   people.pop();
+   document.getElementById("demo").innerHTML = people;
+}
+
+function myPush ()
+{
+   people.push("Candy");
+   document.getElementById("demo").innerHTML = people;
 }
